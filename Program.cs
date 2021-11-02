885a3d57256206c531316b5cfd69061bb085f48a
Ollio <jgg0819@naver.com>
2021-11-03 02:05:06 +0900

    접근 지정자

--- Program.cs
+++ Program.cs
@@ -1,68 +1,32 @@
 ﻿using System;
 
 namespace CSharp
-{
-
-    //객체(OOP Object oriented Programing)
-
-    //Kninght
-    // 속성: hp, attack, pos
-    // 기능: Move, Attack, Die
-
-    //클래스는 참조. 
-
-    class Player // 부모 클래스 혹은 기반 클래스.
+{ 
+    class Knight
     {
-        static public int counter = 1; //오로지 1개만 존재.
-        public int id;
-        public int hp;
-        public int attack;
+        //접근한정자.
+        //public protected private(default는 private)
+        protected int hp;
 
-        public Player()
+        public void SecretFunction()
         {
-            Console.WriteLine("Player 생성자 호출");
+            hp = 20;
         }
 
-        public Player(int hp)
+        public void SetHp(int hp)
         {
             this.hp = hp;
-            Console.WriteLine("Player hp 생성자 호출");
         }
+    }
 
-
-        public void Move()
+       
+    class SuperKnight : Knight
+    {
+        void Test()
         {
-            Console.WriteLine("Knight Move");
+            hp = 10;
         }
-
-        public void Attack()
-        {
-            Console.WriteLine("Knight Attack");
-        }
-
-
     }
-
-    class Mage : Player
-    {
-
-
-    }
-
-    class Archer : Player
-    {
-
-
-
-    }
-
-    class Knight : Player 
-    {
-
-
-    }
-
-
 
     class Program
     {
@@ -72,7 +36,8 @@ namespace CSharp
         {
 
             Knight knight = new Knight();
-            knight.Move();
+            knight.SetHp(100);
+
             
         }
     }
