302be55d84e8b3fc0c4be1f9998f28d2b00e4df7
Ollio <jgg0819@naver.com>
2021-11-27 22:32:07 +0900

    TextRPG 최상위 부모클래스 생성
    Create creature class and Knight damage to mosnter

---
+++ Creature.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharp
+{
+    class Creature
+    {
+        CreatureType type;
+     
+        public enum CreatureType
+        {
+            None,
+            Player = 1,
+            Monster = 2
+        }
+
+        protected Creature(CreatureType type)
+        {
+            this.type = type;
+        }
+
+        protected int hp;
+        protected int attack;
+
+        public void SetInfo(int hp, int attack)
+        {
+            this.hp = hp;
+            this.attack = attack;
+
+        }
+
+        public int GetHp() { return hp; }
+        public int GetAttack() { return attack; }
+
+        public bool IsDead() { return hp <= 0; }
+
+        public void OnDamaged(int damage)
+        {
+            hp -= damage;
+            if (hp < 0)
+            {
+                hp = 0;
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -2,30 +2,18 @@
 
 namespace CSharp
 {
-
+    
   
     class Program
     {
 
         static void Main(string[] args)
         {
-            string name = "Harry Potter";
+            Player player = new Knight();
+            Monster monster = new Orc();
 
-            //1.찾기.
-            bool found = name.Contains("Harry");
-            int index = name.IndexOf('P');
-
-            //2.변형
-            name = name + "Junior";
-
-            string lowerCaseName = name.ToLower();
-            string upperCaseName = name.ToUpper();
-
-           string newName = name.Replace('r','l');
-
-            //3. 분할
-            string[] names = name.Split(new char[] { ' ' });
-            string substringName = name.Substring(5);
+            int damage = player.GetAttack();
+            monster.OnDamaged(damage);
         }
     }
 }
