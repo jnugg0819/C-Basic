d1dacf5f8184a2f4b7cff2153c0b83e50ed3871d
Ollio <jgg0819@naver.com>
2021-11-27 21:51:26 +0900

    문자열 연습

--- Program.cs
+++ Program.cs
@@ -3,85 +3,29 @@
 namespace CSharp
 {
 
-    class Player
-    {
-        protected int hp;
-        protected int attack;
-
-        public virtual void Move()
-        {
-            Console.WriteLine("Player 이동!");
-        }
-    }
-
-    //오버로딩 (함수이름의 재사용) 
-    //오버라이딩 (다형성이용) virtual keyword.(모든함수를 vitual로 한다는것은 비추)
-    class Knight : Player
-    {
-        //sealed는 Knight를 상속받는 자식들은 더이상 건들지 말라는뜻.(TIP). 
-        public override void Move()
-        {
-            base.Move();
-            Console.WriteLine("Knight 이동!");
-        }
-    }
-
-    class Mage : Player
-    {
-        public int mp;
-
-        public override void Move()
-        {
-            Console.WriteLine("Mage 이동!");
-        }
-    }
-
-    class SuperKnight : Knight
-    {
-        public override void Move()
-        {
-            Console.WriteLine("SuperKnight 이동!");
-        }
-    }
-
+  
     class Program
     {
 
-        static Player FindPlayerById(int id)
-        {
-            //id에해당하는 플레이어 탐색.
-            //못찾았을시엔 null.
-            return null;
-        }
-        static void EnterGame(Player player)
-        {
-            player.Move();
-            Mage mage = (player as Mage);
-            if (mage != null) {
-                mage.mp = 10;
-                mage.Move();
-            }
-           
-        }
-    
-
         static void Main(string[] args)
         {
-            Knight knight = new Knight();
-            Mage mage = new Mage();
+            string name = "Harry Potter";
 
-            //Mage타입이 플레이어타입으로 될수있는데 플레이어타입은 Mage타입으로 변환 바로 불가-> 타입캐스팅 해야됨 -> 하지만 해.
-            //Player magePlayer = mage;
-            //Mage mage2 = (Mage)magePlayer;
+            //1.찾기.
+            bool found = name.Contains("Harry");
+            int index = name.IndexOf('P');
 
-            //knight.Move();
-            //mage.Move();
+            //2.변형
+            name = name + "Junior";
 
-            knight.Move();
+            string lowerCaseName = name.ToLower();
+            string upperCaseName = name.ToUpper();
 
-            //EnterGame(mage);
-            //EnterGame(mage);
+           string newName = name.Replace('r','l');
 
+            //3. 분할
+            string[] names = name.Split(new char[] { ' ' });
+            string substringName = name.Substring(5);
         }
     }
 }
