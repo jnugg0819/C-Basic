82974f822250ac827e3dd55daf72ef112644ba93
Ollio <jgg0819@naver.com>
2021-11-27 23:15:43 +0900

    TextRPG 필드입장 코드 추가

--- Game.cs
+++ Game.cs
@@ -17,6 +17,8 @@ namespace CSharp
     {
         private GameMode mode = GameMode.Lobby;
         private Player player = null;
+        private Monster monster = null;
+        private Random rand = new Random();
 
         public void Process()
         {
@@ -29,6 +31,7 @@ namespace CSharp
                     ProcessTown();
                     break;
                 case GameMode.Field:
+                    ProcessField();
                     break;
             }
         }
@@ -73,6 +76,87 @@ namespace CSharp
                     break;       
             }
 
+        }
+
+        private void ProcessField()
+        {
+            Console.WriteLine("필드에 입장했습니다.!");
+            Console.WriteLine("[1] 싸우기");
+            Console.WriteLine("[2] 일정 확률로 마을로 돌아가기");
+
+            CreateRandomMonster();
+
+            string input = Console.ReadLine();
+            switch (input)
+            {
+                case "1":
+                    ProcessFight();
+                    break;
+                case "2":
+                    TryEscape();
+                    break;
+            }
+        }
+
+
+        private void TryEscape()
+        {
+            int randValue = rand.Next(0, 101);
+            if(randValue < 33)
+            {
+                mode = GameMode.Town;
+            }
+            else
+            {
+                ProcessFight();
+            }
+        }
+        private void CreateRandomMonster()
+        {
+
+            int randValue = rand.Next(0, 3);
+            switch (randValue)
+            {
+                case 0:
+                    monster = new Slime();
+                    Console.WriteLine("슬라임이 생성되었습니다.!");
+                    break;
+                case 1:
+                    monster = new Orc();
+                    Console.WriteLine("오크가 생성되었습니다.!");
+                    break;
+                case 2:
+                    monster = new Skeleton();
+                    Console.WriteLine("해골이 생성되었습니다.!");
+                    break;
+            }
+        }
+
+        private void ProcessFight()
+        {
+            while (true)
+            {
+                int damage = player.GetAttack();
+                monster.OnDamaged(damage);
+
+                if (monster.IsDead())
+                {
+                    Console.WriteLine("승리했습니다.");
+                    Console.WriteLine($"남은 체력{player.GetHp()}");
+                    break;
+                }
+
+                damage = monster.GetAttack();
+                player.OnDamaged(damage);
+
+                if (player.IsDead())
+                {
+                    Console.WriteLine("패배했습니다.");
+                    mode = GameMode.Lobby;
+                    break;
+                }
+
+            }
         }
     }
 }
